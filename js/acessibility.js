60e00e864bea5ab16dcbddeb6af1e1d670d40b4d
kyotodevindie <kyotodevindie@gmail.com>
2019-12-01 12:31:33 -0300

    ae po

--- js/acessibility.js
+++ js/acessibility.js
@@ -3,27 +3,34 @@
 
 
 $(document).ready(function(){
-    $("#contraste").click(function(){
-      $("body").toggleClass("contraste");
-      $("a").toggleClass("contraste");
-      $(".contrastex").toggleClass("contraste");
-      $(".contrastef").toggleClass("contrasteimportant");
-      $("p").toggleClass("contraste");
-      $("h6").toggleClass("contraste");
-      $("h5").toggleClass("contraste");
-      $("h4").toggleClass("contraste");
-      $("h3").toggleClass("contraste");
-      $("h2").toggleClass("contraste");
-      $("div").toggleClass("contraste");
-    });
+  $("#contraste").click(function(){
+    $("body").toggleClass("contraste");
+    $("a").toggleClass("contraste");
+    $("p").toggleClass("contraste");
+    $("h6").toggleClass("contraste");
+    $("h5").toggleClass("contraste");
+    $("h4").toggleClass("contraste");
+    $("h3").toggleClass("contraste");
+    $("h2").toggleClass("contraste");
+    $("h1").toggleClass("contraste");
+    $("div").toggleClass("contraste");
+    $(".contrastex").toggleClass("contraste");
+    $(".contrastef").toggleClass("contrasteimportant");
   });
+});
 
-  $(document).ready(function(){
-    $("#fonte").click(function(){
-      $("body").toggleClass("aumentarfonte");
-      $("p").toggleClass("aumentarfonte");
-      $("h5").toggleClass("aumentarfonte");
-      $("h6").toggleClass("aumentarfonte");
-      $("a").toggleClass("aumentarfonte");
-    });
+$(document).ready(function(){
+  $("#fonte").click(function(){
+    $("body").toggleClass("aumentarfonte");
+    $("a").toggleClass("aumentarfonte");
+    $("p").toggleClass("aumentarfonte");
+    $("h6").toggleClass("aumentarfonte");
+    $("h5").toggleClass("aumentarfonte");
+    $("h4").toggleClass("aumentarfonte");
+    $("h3").toggleClass("aumentarfonte");
+    $("h2").toggleClass("aumentarfonte");
+    $("h1").toggleClass("aumentarfonte");
+    $("div").toggleClass("aumentarfonte");
+    $("a").toggleClass("aumentarfonte");
   });
+});
